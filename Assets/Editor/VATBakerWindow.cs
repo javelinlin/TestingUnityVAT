b69072840566ed040c2ab8f76d2c37b03c3a4217
Jave.Lin <linjf_008@126.com>
2024-12-03 09:59:05 +0800

    重构代码

--- Assets/Editor/VATBakerWindow.cs
+++ Assets/Editor/VATBakerWindow.cs
@@ -20,7 +20,8 @@ public class VATBakerWindow : EditorWindow
 
     private AnimationClip clip;
     private GameObject skinGameObjRoot;
-    private float samplingRate = 60.0f;
+    private float samplingRate = 30.0f;
+    private bool ShowSaveFileDialog;
 
     private bool hasBaked = false;
     private Texture2D bakedtexture;
@@ -40,6 +41,7 @@ public class VATBakerWindow : EditorWindow
             (GameObject)EditorGUILayout.ObjectField("Animated GameObject", skinGameObjRoot, typeof(GameObject), true);
         EditorGUILayout.Space();
         samplingRate = EditorGUILayout.FloatField("Sampling rate (times/sec)", samplingRate);
+        ShowSaveFileDialog = EditorGUILayout.Toggle("Show Save File Dialog", ShowSaveFileDialog);
 
         SkinnedMeshRenderer skin = null;
         if (skinGameObjRoot != null)
@@ -75,29 +77,16 @@ public class VATBakerWindow : EditorWindow
     public void GenerateBakedTexture()
     {
         var skinRenderer = skinGameObjRoot.GetComponentInChildren<SkinnedMeshRenderer>();
-        
+
         // ===========================
-        // jave.lin : 计算 需要烘焙的纹理尺寸 : {frameCount} x {vertexCount}
+        // jave.lin : 计算 需要烘焙的纹理尺寸 : texSize {texWidth} x {texHeight} = {frameCount} x {vertexCount}
         // ===========================
-        int vertexCount = skinRenderer.sharedMesh.vertexCount;
-        if (vertexCount > MAX_TEXTURE_SIZE)
+        int vertexCount, frameCount;
+        if (CalculateVertexCountAndFrameCount(skinRenderer, out vertexCount, out frameCount))
         {
-            var msg =
-                $"The Num of Model's ({skinRenderer.sharedMesh.name}) Vertex can not more than : {MAX_TEXTURE_SIZE}";
-            EditorUtility.DisplayDialog("Error", msg, "OK");
             return;
         }
 
-        // jave.lin : texture width 对应 动画时长 * 动画采样率 = 总帧数 
-        int frameCount = Mathf.CeilToInt(clip.length * samplingRate);
-        if (frameCount > MAX_TEXTURE_SIZE)
-        {
-            var msg =
-                $"The Num of BakedFrameCount : (clip_dur({clip.length}) * sampleRate({samplingRate} = {frameCount})) can not more than : {MAX_TEXTURE_SIZE}";
-            EditorUtility.DisplayDialog("Error", msg, "OK");
-            return;
-        }
-        
         // ===========================
         // jave.lin : 获取顶点缓存数据
         // ===========================
@@ -105,80 +94,70 @@ public class VATBakerWindow : EditorWindow
         // jave.lin : 保存在纹理的所有帧的顶点坐标 [frameIDX][vertexIDX]
         // 贴图的 横向对应 : 帧ID， 纵向对应 : 顶点ID
         Vector3[][] vertexPosBuffer_WriteToTex = new Vector3[frameCount][];
-        Mesh bakedMesh = new Mesh();
-
         // Undo.RegisterFullObjectHierarchyUndo(animatedGameObject, "Sample animation");
-
-        // jave.lin : 开始烘焙
-        // jave.lin : 将对应 skinned mesh renderer 下的坐标系转换到 root 坐标系下
-        var toRootMatrix = skinGameObjRoot.transform.worldToLocalMatrix * skinRenderer.transform.localToWorldMatrix;
-        // jave.lin : 根据采样率计算出每次迭代的 delta time
-        var deltaTime = 1.0f / samplingRate;
-        // jave.lin : 按照帧率来烘焙
-        var vertexHelper = new List<Vector3>();
-        for (int frameIDX = 0; frameIDX < frameCount; frameIDX++)
-        {
-            // jave.lin : 修改 skinned mesh renderer 中的内存网格
-            clip.SampleAnimation(skinGameObjRoot, deltaTime * frameIDX);
-            // jave.lin : 将修改后的 skinned mesh renderer 网格模型坐标另一个 mesh 中
-            skinRenderer.BakeMesh(bakedMesh);
-            // jave.lin : 根据烘焙的结果提起 顶点坐标
-            bakedMesh.GetVertices(vertexHelper);
-            // jave.iln : 将每个顶点坐标转换到 root 坐标系下
-            for (int vertexIDX = 0; vertexIDX < vertexHelper.Count; vertexIDX++)
-            {
-                vertexHelper[vertexIDX] = toRootMatrix.MultiplyPoint(vertexHelper[vertexIDX]);
-            }
-
-            // jave.lin : 塞到 备写入 的
-            vertexPosBuffer_WriteToTex[frameIDX] = vertexHelper.ToArray();
-        }
-
+        BakedVertexPositionBuffer(skinRenderer, frameCount, vertexPosBuffer_WriteToTex);
         // Undo.PerformUndo();
 
         // ===========================
-        // jave.lin : 开始填充烘焙的纹理数据
+        // jave.lin : 生成填充烘焙的纹理数据
         // ===========================
-        // jave.lin : 没有 RGBHalf，只有 RGBAHalf，白白浪费了 A 通道的数据
-        Texture2D texture = new Texture2D(
-            frameCount,
-            vertexCount,
-            TextureFormat.RGBAHalf,
-            false, true);
-        texture.filterMode = FilterMode.Bilinear;
-        texture.anisoLevel = 0;
-        for (int frameIDX = 0; frameIDX < frameCount; frameIDX++)
+        var texture = GenerateBakedTextureByVertexPosBuffer(frameCount, vertexCount, vertexPosBuffer_WriteToTex);
+
+        // ===========================
+        // jave.lin : 确定保存烘焙纹理的路径
+        // ===========================
+        var meshName = skinRenderer.sharedMesh.name;
+        var clipName = clip.name;
+        if (MakeSureTexSavePath(meshName, clipName, texture, out var path))
         {
-            for (int vertexIDX = 0; vertexIDX < vertexPosBuffer_WriteToTex[frameIDX].Length; vertexIDX++)
-            {
-                var pos = vertexPosBuffer_WriteToTex[frameIDX][vertexIDX];
-                var saveVal = new Color(pos.x, pos.y, pos.z);
-                texture.SetPixel(frameIDX, vertexIDX, saveVal);
-            }
+            return;
         }
-        texture.Apply();
 
         // ===========================
         // jave.lin : 保存烘焙纹理
         // ===========================
-        string path = EditorUtility.SaveFilePanelInProject(
-            "Save Texture",
-            $"{skinRenderer.sharedMesh.name}_VATTexture_{clip.name}",
-            "asset",
-            "Select destination");
-
-        if (string.IsNullOrEmpty(path))
-        {
-            EditorUtility.DisplayDialog("Error", "Path is invalid!", "OK");
-            DestroyExt(texture);
-            return;
-        }
         AssetDatabase.DeleteAsset(path);
         AssetDatabase.CreateAsset(texture, path);
 
         // ===========================
-        // jave.lin : 创建 VATBaked_skinName 的GO显示在旁边
+        // jave.lin : 创建 VATBaked_skinName 的GO显示在旁边做测试显示效果用
         // ===========================
+        InitiateVatGO4Testing(skinRenderer, texture);
+
+        // ===========================
+        // jave.lin : 将原来的 skinRendererGO 里面的 animator 设置默认的 animator，方便播放直接查看
+        // ===========================
+        SetSrcObjectAnimatorDefaultState();
+
+        // ===========================
+        // jave.lin : 更新已烘焙数据
+        // ===========================
+        hasBaked = true;
+        clipDuration = clip.length;
+        bakedtexture = (Texture2D)AssetDatabase.LoadMainAssetAtPath(path);
+    }
+
+    private void SetSrcObjectAnimatorDefaultState()
+    {
+        var animator = skinGameObjRoot.GetComponent<Animator>();
+        if (animator != null)
+        {
+            AnimatorController animatorController = animator.runtimeAnimatorController as AnimatorController;
+            if (animatorController != null)
+            {
+                // 设定要修改的层索引，0表示默认层
+                int layerIndex = 0;
+                var layer = animatorController.layers[layerIndex];
+
+                // 设置默认状态
+                var defualtState = layer.stateMachine.states.FirstOrDefault(s => s.state.name == clip.name).state;
+                layer.stateMachine.defaultState = defualtState;
+            }
+        }
+    }
+
+    private void InitiateVatGO4Testing(SkinnedMeshRenderer skinRenderer, Texture2D texture)
+    {
         var bakedGOName = $"VATBaked_{skinGameObjRoot.name}";
         var bakedGO = GameObject.Find(bakedGOName);
         bakedGO = bakedGO != null ? bakedGO : new GameObject(bakedGOName);
@@ -198,7 +177,7 @@ public class VATBakerWindow : EditorWindow
         var bakedGOMat = mr.sharedMaterial;
         if (bakedGOMat == null)
         {
-            var matPath = "Assets/TestingArts/Test_TestingUnityVAT_V4.mat";
+            var matPath = "Assets/TestingArts/Materials/Test_TestingUnityVAT_V4.mat";
             bakedGOMat = AssetDatabase.LoadAssetAtPath<Material>(matPath);
             if (bakedGOMat == null)
             {
@@ -214,34 +193,134 @@ public class VATBakerWindow : EditorWindow
         mr.sharedMaterial = bakedGOMat;
         bakedGOMat.SetTexture("_MainTex", src_mat.GetTexture("_MainTex"));
         bakedGOMat.SetTexture("_VATTex", texture);
-        bakedGOMat.SetFloat("_Duration", clip.length);
-        bakedGOMat.SetFloat("_FPS", samplingRate);
-        
-        // ===========================
-        // jave.lin : 将原来的 skinRendererGO 里面的 animator 设置默认的 animator，方便播放直接查看
-        // ===========================
-        var animator = skinGameObjRoot.GetComponent<Animator>();
-        if (animator != null)
-        {
-            AnimatorController animatorController = animator.runtimeAnimatorController as AnimatorController;
-            if (animatorController != null)
-            {
-                // 设定要修改的层索引，0表示默认层
-                int layerIndex = 0;
-                var layer = animatorController.layers[layerIndex];
+        // "R : Duration, G : FPS, B : PlayTimeOffset, A : IsLoop"
+        bakedGOMat.SetVector(
+            "_PackData0",
+            new Vector4(
+                clip.length, // R : Duration
+                samplingRate, // G : FPSG : FPS
+                0, // PlayTimeOffset
+                1 // IsLoop
+            ));
+    }
 
-                // 设置默认状态
-                var defualtState = layer.stateMachine.states.FirstOrDefault(s => s.state.name == clip.name).state;
-                layer.stateMachine.defaultState = defualtState;
+    private bool MakeSureTexSavePath(string meshName, string clipName, Texture2D texture, out string path)
+    {
+        var saveFileName = $"{meshName}_VATTexture_{clipName}.asset";
+        if (ShowSaveFileDialog)
+        {
+            path = EditorUtility.SaveFilePanelInProject(
+                "Save Texture",
+                saveFileName,
+                "asset",
+                "Select destination");
+        }
+        else
+        {
+            path = $"Assets/TestingArts/BakedTexs/{saveFileName}";
+        }
+
+
+        if (string.IsNullOrEmpty(path))
+        {
+            EditorUtility.DisplayDialog("Error", $"Path is invalid : {path}", "OK");
+            DestroyExt(texture);
+            return true;
+        }
+
+        return false;
+    }
+
+    private static Texture2D GenerateBakedTextureByVertexPosBuffer(
+        int frameCount,
+        int vertexCount,
+        Vector3[][] result)
+    {
+        // jave.lin : 没有 RGBHalf，只有 RGBAHalf，白白浪费了 A 通道的数据
+        Texture2D texture = new Texture2D(
+            frameCount,
+            vertexCount,
+            TextureFormat.RGBAHalf,
+            false, true);
+        texture.filterMode = FilterMode.Bilinear;
+        texture.anisoLevel = 0;
+        for (int frameIDX = 0; frameIDX < frameCount; frameIDX++)
+        {
+            for (int vertexIDX = 0; vertexIDX < result[frameIDX].Length; vertexIDX++)
+            {
+                var pos = result[frameIDX][vertexIDX];
+                var saveVal = new Color(pos.x, pos.y, pos.z);
+                texture.SetPixel(frameIDX, vertexIDX, saveVal);
             }
         }
 
-        // ===========================
-        // jave.lin : 更新已烘焙数据
-        // ===========================
-        hasBaked = true;
-        clipDuration = clip.length;
-        bakedtexture = (Texture2D)AssetDatabase.LoadMainAssetAtPath(path);
+        texture.Apply();
+        return texture;
+    }
+
+    private void BakedVertexPositionBuffer(
+        SkinnedMeshRenderer skinRenderer,
+        int frameCount,
+        Vector3[][] result)
+    {
+        // jave.lin : 开始烘焙
+        // jave.lin : 将对应 skinned mesh renderer 下的坐标系转换到 root 坐标系下
+        var toRootMatrix = skinGameObjRoot.transform.worldToLocalMatrix *
+                           skinRenderer.transform.localToWorldMatrix;
+        // jave.lin : 根据采样率计算出每次迭代的 delta time
+        var deltaTime = 1.0f / samplingRate;
+        // jave.lin : 按照帧率来烘焙
+        var vertexHelper = new List<Vector3>();
+        Mesh bakedMesh = new Mesh();
+        for (int frameIDX = 0; frameIDX < frameCount; frameIDX++)
+        {
+            // jave.lin : 修改 skinned mesh renderer 中的内存网格
+            clip.SampleAnimation(skinGameObjRoot, deltaTime * frameIDX);
+            // jave.lin : 将修改后的 skinned mesh renderer 网格模型坐标另一个 mesh 中
+            skinRenderer.BakeMesh(bakedMesh);
+            // jave.lin : 根据烘焙的结果提起 顶点坐标
+            bakedMesh.GetVertices(vertexHelper);
+            // jave.iln : 将每个顶点坐标转换到 root 坐标系下
+            for (int vertexIDX = 0; vertexIDX < vertexHelper.Count; vertexIDX++)
+            {
+                vertexHelper[vertexIDX] = toRootMatrix.MultiplyPoint(vertexHelper[vertexIDX]);
+            }
+
+            // jave.lin : 塞到 备写入 的
+            result[frameIDX] = vertexHelper.ToArray();
+        }
+
+        DestroyExt(bakedMesh);
+    }
+
+    private bool CalculateVertexCountAndFrameCount(
+        SkinnedMeshRenderer skinRenderer,
+        out int vertexCount,
+        out int frameCount)
+    {
+        vertexCount = 0;
+        frameCount = 0;
+
+        vertexCount = skinRenderer.sharedMesh.vertexCount;
+        if (vertexCount <= 0 || vertexCount > MAX_TEXTURE_SIZE)
+        {
+            var msg =
+                $"The Num of Model's ({skinRenderer.sharedMesh.name}) VertexCount is : {vertexCount}, it must in range : [{1}~{MAX_TEXTURE_SIZE}]";
+            EditorUtility.DisplayDialog("Error", msg, "OK");
+            return true;
+        }
+
+        // jave.lin : texture width 对应 动画时长 * 动画采样率 = 总帧数 
+        frameCount = Mathf.CeilToInt(clip.length * samplingRate);
+        if (frameCount <= 0 || frameCount > MAX_TEXTURE_SIZE)
+        {
+            var msg =
+                $"The Num of Sampling Animation State: {clip.name}) FrameCount {frameCount}, it must in range : [{1}~{MAX_TEXTURE_SIZE}]";
+            EditorUtility.DisplayDialog("Error", msg, "OK");
+            return true;
+        }
+
+        return false;
     }
 
     private static void DestroyExt(UnityEngine.Object obj)
